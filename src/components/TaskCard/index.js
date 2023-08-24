c2a40becd7d2807e9eb9dcaaed5f4825c4dcefd0
Pedro Cardoso <pedrocastilhoweb@gmail.com>
2023-08-24 02:38:07 -0300

    :sparkles: feat: connecting to backend and getting tasks

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { format } from 'date-fns'
 import * as S from './styles'
 
 import iconDefault from '../../assets/default.png'
 
-function TaskCard() {
+function TaskCard({ type, title, when }) {
+  const date = useMemo(() => format(new Date(when), 'dd/MM/yyyy'))
+  const hour = useMemo(() => format(new Date(when), 'HH:mm'))
+  
   return (
     <S.Container>
       <S.TopCard>
         <img src={iconDefault} alt="ícone da tarefa" />
-        <h3>Título da tarefa</h3>
+        <h3>{title}</h3>
       </S.TopCard>
     <S.BottomCard>
-        <strong>23/08/2023</strong>
-        <span>22:00</span>
+        <strong>{date}</strong>
+        <span>{hour}</span>
     </S.BottomCard>
     </S.Container>
   )
